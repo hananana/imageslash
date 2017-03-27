6d21378c35576325b1295b84e239a054a111a8e6
hananana <hananana.vimmer@gmail.com>
2017-03-27 22:55:26 +0900

    :new: add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+# coding: UTF-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='imageslash',
+    version='1.0',
+    packages=find_packages(),
+    description='cut mapchip',
+    author='hananana',
+    license='MIT',
+    install_requires=[
+        'click',
+    ],
+    entry_points='''
+        [console_scripts]
+        imageslash=src:cmd
+    ''',
+)
