a8ea9b629815251cecbb6b7066bbbb8db3092837
hananana <hananana.vimmer@gmail.com>
2017-03-27 22:44:30 +0900

    :new: add imageslash.py

---
+++ src/imageslash.py
@@ -0,0 +1,20 @@
+# coding: UTF-8
+
+import click
+
+
+@click.command()
+def cmd():
+    pass
+
+
+def copy_unity_project(path):
+    pass
+
+
+def check_platform(platform):
+    pass
+
+
+if __name__ == '__main__':
+    cmd()
