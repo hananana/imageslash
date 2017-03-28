933d859ff958fcd6a59199b1fc31dcd0f0a4ad32
hananana <hananana.vimmer@gmail.com>
2017-03-28 22:11:31 +0900

    :syringe: fix option name

--- src/imageslash.py
+++ src/imageslash.py
@@ -5,13 +5,13 @@ from PIL import Image
 
 
 @click.command()
-@click.option('--target', '-t', type=click.Path(exists=True))
+@click.option('--input', '-i', type=click.Path(exists=True))
 @click.option('--left', '-l', type=int)
 @click.option('--top', '-t', type=int)
 @click.option('--right', '-r', type=int)
 @click.option('--bottom', '-b', type=int)
 @click.option('--output', '-o')
-def cmd(target, left, top, right, bottom, output):
-    img = Image.open(target)
+def cmd(input, left, top, right, bottom, output):
+    img = Image.open(input)
     cropped = img.crop((left, top, right, bottom))
     cropped.save(output, 'PNG')
